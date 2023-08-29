f5680203c1b141b3099990c5a38d3ba7d27d52c8
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-08-29 15:50:29 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const path = require('path');
 
 const { dbConnection } = require('./database/config');
 
@@ -30,6 +32,11 @@ app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/uploads', require('./routes/uploads') );
 
+
+// Lo ultimo
+app.get('*', ( req, res ) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) )
+});
 
 
 app.listen( process.env.PORT, () => {
